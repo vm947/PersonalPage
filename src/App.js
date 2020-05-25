8b0611b0980c5a7f4c04401fefecc7813d70ed4d
vm947 <vm947@nyu.edu>
2020-05-25 10:25:53 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Jumbo from './components/Jumbo';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Works from './components/Works';
 import { Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
 
@@ -10,7 +11,10 @@ function App() {
     <div className="App">
       <Jumbo/>
       <Navbar/>
+      <Switch>
+      <Route exact path ='/works' component={Works}></Route>
       <Route exact path='/'component={Home}></Route>
+      </Switch>
     </div>
   );
 }
