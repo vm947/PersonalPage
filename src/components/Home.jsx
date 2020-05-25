4b6a02188f33a046814b552512ef3758e5fdb766
vm947 <vm947@nyu.edu>
2020-05-25 15:29:54 +0300

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,11 @@ import React from "react";
 export const Home = () => {
   return (
     <div className="home-container">
-      <h1>Hello Vincent</h1>
-     
+      <h1>Welcone</h1>
+      <p className='p-home'>
+        Welcome to this homepage. It is a React practice homepage where I will
+        try to update with future practice projects!
+      </p>
     </div>
   );
 };
